4da0990a834a8236a16c5d4cab2f43ca64a5b455
SepteRR-dev <fabianrajmund@onet.pl>
2021-01-27 21:15:41 +0100

    OnDeath Event (Spectator after Death)

--- src/main/java/aqass/uhc/Uhc.java
+++ src/main/java/aqass/uhc/Uhc.java
@@ -1,9 +1,15 @@
 package aqass.uhc;
 
 import aqass.uhc.listener.regenerationListener;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Uhc extends JavaPlugin {
+
+public final class Uhc extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
@@ -19,7 +25,20 @@ public final class Uhc extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
 
-        System.out.println("Plugin is dead lmao");
+        System.out.println("Plugin is dead lmao"); //Oh yyes bull
+
+
+    }
+
+    @EventHandler
+    public void OnDeath(PlayerDeathEvent event){
+
+        Player player = event.getEntity().getPlayer();
+        if (player.isDead()){
+
+            player.setGameMode(GameMode.SPECTATOR);
+
+        }
 
     }
 
