c5a0a8d968affd01efea7be60861638e4c8ea302
SepteRR-dev <fabianrajmund@onet.pl>
2021-01-27 22:53:59 +0100

    OnDeath(Spectator)

--- src/main/java/aqass/uhc/Uhc.java
+++ src/main/java/aqass/uhc/Uhc.java
@@ -2,11 +2,7 @@ package aqass.uhc;
 
 import aqass.uhc.listener.deathListener;
 import aqass.uhc.listener.regenerationListener;
-import org.bukkit.GameMode;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
