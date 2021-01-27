1012b3d64535496c632ba0c6964e216870ef9d13
satook.dev <satook.dev@gmail.com>
2021-01-27 08:03:26 +0100

    first commit

---
+++ src/main/java/aqass/uhc/Uhc.java
@@ -0,0 +1,17 @@
+package aqass.uhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Uhc extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
